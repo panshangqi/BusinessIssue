850027414c7d2a4bffaf946f510b792aacfeefc1
geyingqi <geyingqi@kuailexue.com>
2018-01-11 14:34:54 +0800

    base complete

---
+++ base_handler.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time  :  2018/1/11 下午12:40
+
+import logging
+from tornado.web import RequestHandler
+
+
+class BaseHandler(RequestHandler):
+    def get(self):
+        logging.info('show me the data')
+        self.write()
+
+
+class DownloadHandler(RequestHandler):
+    def post(self):
+        pass
+
---
+++ routes.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time  :  2018/1/11 上午11:12
+
+
+def get():
+    routes = [
+        (r'/business_issues', 'base_handler.BaseHandler'),
+    ]
+    return routes
