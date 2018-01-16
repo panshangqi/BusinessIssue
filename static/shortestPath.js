8cb3565b8ba595dae168e62ac81139d4acb0480b
songfengjuan <fengjuan.song@17zuoye.com>
2018-01-16 10:03:13 +0800

    update

--- static/shortestPath.js
+++ static/shortestPath.js
@@ -213,11 +213,11 @@ $(function() {
                 d:unitVal,
                 points:arrJson
             }
-            iniChart(currentHeigh,currentWidth,currentX,currentY,1,newArr,false,pathTitle,true);
-            $(".short-path").html('23cm');
-            fillListHtml(newArr);
-            $(this).addClass("done");
-            var pathTitle = '最短路径为24cm';
+            // iniChart(currentHeigh,currentWidth,currentX,currentY,1,newArr,false,pathTitle,true);
+            // $(".short-path").html('23cm');
+            // fillListHtml(newArr);
+            // $(this).addClass("done");
+            // var pathTitle = '最短路径为24cm';
             if(dotArr.length){
                 if(dotArr.length<3){
                     alert('请选中三个点以上');
